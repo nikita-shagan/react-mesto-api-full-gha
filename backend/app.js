a2c0fefac1d2be2775365dd7c729e0f684fa2484
nikita-shagan <nikita.ydisk@yandex.ru>
2023-06-23 07:30:50 +0000

    refactor: change mongo ip

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
